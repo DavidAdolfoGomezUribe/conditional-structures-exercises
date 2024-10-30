3d58f1b4e3a4061a01fbab1cdaa6bc8cd8fb9eab
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-10-30 17:29:26 -0500

    Solucion del ejercicio Determinar par

---
+++ main.py
@@ -0,0 +1,53 @@
+#Escriba un programa que determine si el número entero ingresado por el usuario es par o no.
+#
+#Ingrese un número: 4
+#Su número es par
+#Ingrese un número: 3
+#Su número es impar
+
+
+import math
+
+
+print(f""" 
+██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝                                                        
+                                """) 
+name = input("    Hello, please enter your full name:  ")
+
+while True:
+
+    print(f""" \n
+        Welcome back Mr/Ms {name}, this is a program for calculate if the numeber is odd of even \n 
+          """) 
+
+
+
+    try :    
+        
+        aNumber = float(input("        Pleas enter the number: "))
+
+        if aNumber % 2 == 0 :
+              print ("        The number is even")
+        
+        else :
+              print ("        The number is odd")
+
+
+
+        continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
+
+        if continueAsk != "yes" :
+                print("    Thaks for using the program. Goodbye! \n ")
+                break
+    except :            
+        print("\n        Error: Please enter the correct data ")
+        continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
+
+        if continueAsk != "yes" :
+                print("    Thaks for using the program. Goodbye! \n ")
+                break
