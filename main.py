483848dbc1c8bbf661291303055ad29d6d65258b
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-10-30 22:52:09 -0500

    Solucion del ejercicio Ordenamiento

--- main.py
+++ main.py
@@ -1,58 +1,84 @@
-#Palabra más larga
-#Escriba un programa que pida al usuario dos palabras, 
-#y que indique cuál de ellas es la más larga y por cuántas letras lo es.
+#Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
 #
-#Palabra 1: edificio
-#Palabra 2: tren
-#La palabra edificio tiene 4 letras mas que tren.
-#Palabra 1: sol
-#Palabra 2: paralelepipedo
-#La palabra paralelepipedo tiene 11 letras mas que sol
-#Palabra 1: plancha
-#Palabra 2: lapices
-#Las dos palabras tienen el mismo largo
+#Ingrese numero: 51
+#Ingrese numero: 24
+#24 51
+#A continuación, escriba otro programa que haga lo mismo con tres números:
+#
+#Ingrese numero: 8
+#Ingrese numero: 1
+#Ingrese numero: 4
+#1 4 8
+#Finalmente, escriba un tercer programa que ordene cuatro números:
+#
+#Ingrese numero: 7
+#Ingrese numero: 0
+#Ingrese numero: 6
+#Ingrese numero: 1
+#0 1 6 7
+#Recuerde que su programa debe entregar la solución correcta para cualquier combinación de números,
+#no sólo para los ejemplos mostrados aquí.
+#
+#Hay más de una manera de resolver cada ejercicio.
 
 import math
 
 
 print(f""" 
-██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
-██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
-██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
-██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
-╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
- ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝                                                        
+    ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+    ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+    ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+    ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+    ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+     ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝                                                        
                                 """) 
 name = input("    Hello, please enter your full name:  ")
 
 while True:
 
     print(f""" \n
-        Welcome back Mr/Ms {name}, this is a program for calculate the length of 2 words and how to kwon
-        which one have more letters \n
+        Welcome back Mr/Ms {name}, this is a program for calculate order of a certain amount of numbers \n
               """) 
 
     try :    
         
-        wordOne = str(input("        Please enter the first word: "))
-        wordTwo = str(input("        Please enter the second second: " ))
-
-        nWordOne = len(wordOne)
-        nWordTwo = len(wordTwo)
         
-        if nWordOne > nWordTwo :
-            words = nWordOne - nWordTwo
-            print(f"        The word {wordOne} have {words} more letters than the word {wordTwo}")
+        select = int(input("        Select the amount of numbers that you want to order (2 , 3 or 4 numbers) : "))
+        if select == 2 :
+            numberOne  =  float(input("        Enter your first number : "))
+            numberTwo  =  float(input("        Enter your second number : "))
+            
+            array = [numberOne , numberTwo]
+            array.sort()
+            print("        The order of the numbers is : ",", " .join(map(str,array))) #para eliminar los corchetes de un array y separar los elementos del array con comas, ademas trasnforma los elemntos de array en un str
+
+
+    
+        if select == 3 :
+            numberOne  =  float(input("        Enter your first number : "))
+            numberTwo  =  float(input("        Enter your second number : "))
+            numberThree = float(input("        Enter your thirdth : "))
+            
+            array = [numberOne , numberTwo, numberThree]
+            array.sort()
+            print("        The order of the numbers is : ",", " .join(map(str,array)))
         
-        if nWordOne < nWordTwo :
-            words =  nWordTwo - nWordOne 
-            print(f"        The word {wordTwo} have {words} more letters than the word {wordOne}")
+        if select == 4 :
+            numberOne  =  float(input("        Enter your first number : "))
+            numberTwo  =  float(input("        Enter your second number : "))
+            numberThree = float(input("        Enter your thirdth : "))
+            numberFour = float(input("        Enter your fourth : "))
 
-        if nWordOne == nWordTwo :
-            print(f"        Both words have se same length")
+            array = [numberOne , numberTwo, numberThree , numberFour]
+            array.sort()
+            print("        The order of the numbers is : ",", " .join(map(str,array)))
 
-
-
+        if select != 2 and select != 3 and select != 4 :    
+            print("\n        Error: Please enter the correct data ")    
+        
+        
+        
+        
         continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
 
         if continueAsk != "yes" :
